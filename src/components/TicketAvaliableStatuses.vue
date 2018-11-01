9dbde5eb60ac20f802a2cd1c8e7219edc6e63cd0
Tolokonnikov Dmitriy <Dmitriy.Tolokonnikov@alp.ru>
2018-11-01 17:27:53 +0300

    WIP action before ticket new status

--- src/components/TicketAvaliableStatuses.vue
+++ src/components/TicketAvaliableStatuses.vue
@@ -3,7 +3,7 @@
     <template v-for="(status, index) in statuses">
       <v-list-tile @click='onStatusClicked (status)'>
         <v-list-tile-content>
-          <v-list-tile-title>{{status}}</v-list-tile-title>
+          <v-list-tile-title>{{status.name}}</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-divider v-if="index + 1 < statuses.length" :key="`divider-${index}`"></v-divider>
@@ -29,7 +29,11 @@ export default {
   },
   methods: {
     onStatusClicked (status) {
-      this.$store.dispatch('updateTicketStatus', {self: this, status: status})
+      if (!status.actionBeforeStatus) {
+        this.$store.dispatch('updateTicketStatus', {self: this, status: status.name})
+      } else {
+        this.$router.push({name: 'TicketBeforeNewStatus', params: { ticketNumber: this.ticketnumber, status: status.name, actionBeforeStatus: status.actionBeforeStatus }})
+      }
     }
   },
   name: 'TicketAvaliableStatuses'
---
+++ src/components/TicketBeforeNewStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-text-field v-if="actionBeforeStatus.comment"
+    label="Текущий этап"
+    value="123"
+    readonly
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  created () {
+    this.$store.commit('setMainNavbarState', {title: this.status, returnButton: true})
+  },
+  data () {
+    return {
+      ticketNumber: this.$route.params.ticketNumber,
+      status: this.$route.params.status,
+      actionBeforeStatus: this.$route.params.actionBeforeStatus
+    }
+  },
+  name: 'TicketBeforeNewStatus'
+}
+</script>
